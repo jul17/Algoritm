f18aef47e48cf102556056805e12d1fb2f5b4c46
julia bashko <julia0615.jb@gmali.com>
2018-11-14 22:54:17 +0200

    Lab3(Game Server)

--- lab2/src/main/java/PainterManager.java
+++ lab2/src/main/java/PainterManager.java
@@ -5,38 +5,39 @@ public class PainterManager {
     int time;
     int countOfPainters;
 
-    public PainterManager(){
+    public PainterManager() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter count of Painters: ");
         countOfPainters = scan.nextInt();
         System.out.println("Enter Time for painters: ");
         time = scan.nextInt();
-    };
+    }
 
 
 
-    public PainterManager(int time, int countOfPainters){
+
+    public PainterManager(int time, int countOfPainters) {
         this.time = time;
         this.countOfPainters = countOfPainters;
     }
 
-    public int getMaxTime(List<Integer> countOfBigBorders){
+    public int getMaxTime(List<Integer> countOfBigBorders) {
         int max = 0;
-        for(Integer value: countOfBigBorders){
-            if(value>max) {
+        for (Integer value : countOfBigBorders) {
+            if (value > max) {
                 max = value;
             }
         }
         return max * time;
     }
 
-    public List<Integer> getBigbordersForPainters(List<Integer> countOfBigBorders){
-        while(this.countOfPainters != countOfBigBorders.size()){
+    public List<Integer> getBigbordersForPainters(List<Integer> countOfBigBorders) {
+        while (this.countOfPainters != countOfBigBorders.size()) {
             int startIndex = 0;
             int minSum = countOfBigBorders.get(0) + countOfBigBorders.get(1);
-            for(int i = 0; i < countOfBigBorders.size()-1; i++){
-                if(minSum > countOfBigBorders.get(i) + countOfBigBorders.get(i+1)){
-                    minSum = countOfBigBorders.get(i) + countOfBigBorders.get(i+1);
+            for (int i = 0; i < countOfBigBorders.size() - 1; i++) {
+                if (minSum > countOfBigBorders.get(i) + countOfBigBorders.get(i + 1)) {
+                    minSum = countOfBigBorders.get(i) + countOfBigBorders.get(i + 1);
                     startIndex = i;
                 }
             }
@@ -44,7 +45,7 @@ public class PainterManager {
             countOfBigBorders.remove(startIndex + 1);
         }
 
-    return countOfBigBorders;
+        return countOfBigBorders;
     }
 
     public int getCountOfPainters() {
